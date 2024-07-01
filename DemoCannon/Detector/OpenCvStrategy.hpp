d6e70000957a57c1f935feb610016cc64862ec89
김현수/선임연구원/Data플랫폼개발팀(hyunsoo1018.kim@lge.com) <hyunsoo1018.kim@lge.com>
2024-07-01 16:34:27 -0400

    add config cv min, max contour area

--- DemoCannon/Detector/OpenCvStrategy.hpp
+++ DemoCannon/Detector/OpenCvStrategy.hpp
@@ -8,6 +8,10 @@ public:
     virtual void detect(const cv::Mat& Frame);
     virtual void sync(TDetected (&detected)[], int &numDetected);
     void setMinDiffThreshold(double minDiffThreshold);
+    void setMinContourArea(double minContourArea);
+    void setMaxContourArea(double maxContourArea);
 private:
-    double minDiffThreshold = 1500000;
+    double minDiffThreshold = 1000000;
+    double minContourArea = 1000;
+    double maxContourArea = 30000;
 };
